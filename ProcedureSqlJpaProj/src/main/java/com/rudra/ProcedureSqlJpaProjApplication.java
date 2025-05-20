f85b1b6f1d44b6baf8db3fd9497fbefce37a7b7f
rudramadhab22 <rudramadhab007@gmail.com>
2025-05-21 00:33:21 +0530

    new

---
+++ ProcedureSqlJpaProj/src/main/java/com/rudra/ProcedureSqlJpaProjApplication.java
@@ -0,0 +1,13 @@
+package com.rudra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcedureSqlJpaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcedureSqlJpaProjApplication.class, args);
+	}
+
+}
---
+++ ProcedureSqlJpaProj/src/test/java/com/rudra/ProcedureSqlJpaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcedureSqlJpaProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
